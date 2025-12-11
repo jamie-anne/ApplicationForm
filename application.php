00420677d67e470b56cf8d4b4c4a3fa43139ba1f
Jamie Anne <jamie@example.com>
2025-12-11 23:15:58 +0800

    Finally PHP

--- application.php
+++ application.php
@@ -29,17 +29,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $area_volunteer_db = $_POST['area_volunteer_db'] ?? '';
     $activities_volunteer_db = $_POST['activities_volunteer_db'] ?? '';
     $textarea_db = $_POST['textarea_db'] ?? '';
-    $days = $_POST['days'] ?? [];
-    $from_hour_db = $_POST['from-hour_db'] ?? '';
-    $from_ampm_db = $_POST['from_ampm_'] ?? '';
+    $days_db = $_POST['days_db'] ?? [];
+    $from_hour_db = $_POST['from_hour_db'] ?? '';
+    $from_ampm_db = $_POST['from_ampm_db'] ?? '';
     $to_hour_db = $_POST['to-hour_db'] ?? '';
-    $to_ampm_db = $_POST['to_ampm_'] ?? '';
+    $to_ampm_db = $_POST['to_ampm_db'] ?? '';
     $additional_info_db = $_POST['additional_info_db'] ?? '';
     
 
     if ($first_name && $last_name) {
-        $stmt = $conn->prepare("INSERT INTO volunteers (first_name, last_name) VALUES (?, ?)");
-        $stmt->bind_param("ss", $first_name, $last_name);
+        $stmt = $conn->prepare("INSERT INTO volunteers (firstname_db, middlename_db, lastname_db, birthdate_db, email_db, contact_db, province_db,
+        city_db, barangay_db, address_db, college_db, course_db, occupation_db, department_db, area_volunteer_db, activities_volunteer_db
+        textarea_db, days_db, from_hour_db, from_ampm_db, to_hour_db, to_ampm_db, additional_info_db) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sssssssssssssssssssssss", $firstname_db, $middlename_db, $lastname_db, $birthdate_db, $email_db, $contact_db, $province_db,$city_db,
+        $barangay_db, $address_db, $college_db, $course_db, $occupation_db, $department_db, $area_volunteer_db, $activities_volunteer_db,
+        $textarea_db, implode(",", $days_db), $from_hour_db, $from_ampm_db, $to_hour_db, $to_ampm_db, $additional_info_db);
 
         if ($stmt->execute()) {
             header("Location: application.html?success=1");
